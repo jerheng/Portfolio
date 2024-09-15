607801a6faa2e8b3871481550de4a07ee17b13e2
jerheng <57416682+jerheng@users.noreply.github.com>
2024-09-16 01:36:56 +0800

    updates

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
-import VueSmoothScroll from 'vue-smooth-scroll'
 
 const app = createApp(App)
-app.use(VueSmoothScroll, {
-  duration: 500,
-  offset: -80, // Adjust this value based on your navbar height
-})
 app.mount('#app')
